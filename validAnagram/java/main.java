725c69636b18f0570e3de7ca3e026bbcfe25bd43
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2022-12-14 10:20:40 +0530

    Day2

---
+++ validAnagram/java/main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] ch1=s.toCharArray();
+        char[] ch2=t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i] != ch2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
