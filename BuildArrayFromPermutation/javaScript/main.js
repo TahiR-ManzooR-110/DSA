22c4d82e4979dbc88ded0f3756bd0443bd28aa7f
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2022-12-20 23:11:34 +0530

    Day8

---
+++ BuildArrayFromPermutation/javaScript/main.js
@@ -0,0 +1,7 @@
+var buildArray = function(nums) {
+    let res=[];
+    for(let i=0;i<nums.length;i++){
+        res.push(nums[nums[i]]);
+    }
+    return res;
+};
---
+++ BuildArrayFromPermutation/python/main.py
@@ -0,0 +1,8 @@
+from ast import List
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in range(0,len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
