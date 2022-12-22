837e43fb64f8b254c46752db3f56dd4cf917bbbc
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2022-12-22 15:37:08 +0530

    Day9

---
+++ containsDuplicateII/java/main.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i != j && nums[i]==nums[j] && Math.abs(i-j)<=k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
